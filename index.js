c508d3a55634293632192718baf18624f45ee663
lordypopdy-lab <deelordpopdy2@gmail.com>
2024-10-15 21:29:29 +0100

    safe

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(cors({
     origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
-    credentials: true 
   }));
 
 mongoose.connect(process.env.MONGO_URL)

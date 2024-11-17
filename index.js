ed30f69d3e045b1c9970af0b3fbc9e25183a069f
lordypopdy-lab <deelordpopdy2@gmail.com>
2024-11-17 11:08:18 +0100

    safe

--- index.js
+++ index.js
@@ -6,19 +6,6 @@ const cors = require('cors');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://bitclub-wallet.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
-
 mongoose.connect(process.env.MONGO_URL)
 .then(()=> console.log('Database Connected successfuly!'))
 .catch((error)=> console.log('Database not connected', error));
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,6 +3,19 @@ const cors = require("cors");
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://bitclub-wallet.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 const corsOptions = {
   origin: 'https://bitclub-wallet.vercel.app', 
   credentials: true,

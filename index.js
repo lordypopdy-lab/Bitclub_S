83adda20a3fe826a1962cb4182e6e94b079ff38d
lordypopdy-lab <deelordpopdy2@gmail.com>
2024-11-17 10:07:38 +0100

    safe

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const cors = require('cors');
 
 const app = express();
 
+app.use((req, res, next) => {
+  const allowedOrigin = req.headers.origin;
+  res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
+  res.setHeader('Access-Control-Allow-Credentials', 'true'); // If required
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // app.use(cors({
 //     origin: '*', 
 //     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 

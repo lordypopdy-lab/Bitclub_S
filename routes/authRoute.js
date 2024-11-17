01fb0c15f1345fe4710d316da70da9303d70549c
lordypopdy-lab <deelordpopdy2@gmail.com>
2024-11-17 09:30:10 +0100

    safe

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -41,7 +41,7 @@ const {
 
 router.use(
   cors({
-    origin: "https://bitclub-wallet.vercel.app",
+    origin: "https://bitclub-wallet.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
